336149d33116f819a73bc3df224848ade52b866f
Mark <mwemekamp@gmail.com>
2016-04-19 20:40:14 +0200

    Created a directive for the checkboxlist

---
+++ src/RankOne.SEO.Tool/Web/UI/App_Plugins/RankOne/directives/checkboxlist/checkboxlist.directive.js
@@ -0,0 +1,27 @@
+﻿angular.module('umbraco').directive('checkboxlist', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/App_Plugins/RankOne/directives/checkboxlist/checkboxlist.directive.html',
+        scope: {
+            prevalues: '=',
+            model: '='
+        },
+        link: function(scope) {
+            if (!scope.model) {
+                scope.model = [];
+            }
+
+            if (scope.model.length == 0) {
+                angular.forEach(scope.prevalues, function(prevalue) {
+                    scope.model.push(
+                        {
+                            name: prevalue,
+                            checked: true
+                        }
+                    );
+                });
+            }
+        }
+    }
+});
--- src/RankOne.SEO.Tool/Web/UI/App_Plugins/RankOne/prevalue-editors/typeSelector.controller.js
+++ src/RankOne.SEO.Tool/Web/UI/App_Plugins/RankOne/prevalue-editors/typeSelector.controller.js
@@ -4,21 +4,6 @@
     function typeSelectorController($scope) {
 
         $scope.prevalues = ["Error", "Warning", "Hint", "Success"];
-
-        if (!$scope.model.value) {
-            $scope.model.value = [];
-        }
-
-        if ($scope.model.value.length == 0) {
-            angular.forEach($scope.prevalues, function (prevalue) {
-                $scope.model.value.push(
-                    {
-                        name: prevalue,
-                        checked: true
-                    }
-                );
-            }); 
-        }
     };
 
     // Register the controller
