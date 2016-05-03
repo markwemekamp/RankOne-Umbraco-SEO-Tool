bd71d8c9807b1082deb35cee886ce89a58308b52
Mark <mwemekamp@gmail.com>
2016-05-03 13:35:41 +0200

    Added check for head tag to TitleAnalyzer

--- src/RankOne.SEO.Business/Analyzers/Html/TitleAnalyzer.cs
+++ src/RankOne.SEO.Business/Analyzers/Html/TitleAnalyzer.cs
@@ -14,51 +14,58 @@ namespace RankOne.Business.Analyzers.Html
             };
 
             var headTag = HtmlHelper.GetElements(document, "head");
-            var titleTags = HtmlHelper.GetElements(headTag.First(), "title");
-
-            if (!titleTags.Any())
+            if (headTag.Any())
             {
-                result.AddResultRule("titleanalyzer_no_title_tag", ResultType.Error);
-            }
-            else if (titleTags.Count() > 1)
-            {
-                result.AddResultRule("titleanalyzer_multiple_title_tags", ResultType.Error);
-            }
-            else
-            {
-                var firstTitleTag = titleTags.FirstOrDefault();
-                if (firstTitleTag != null)
+                var titleTags = HtmlHelper.GetElements(headTag.First(), "title");
+                if (!titleTags.Any())
                 {
-                    var titleValue = firstTitleTag.InnerText;
-
-                    if (string.IsNullOrWhiteSpace(titleValue))
+                    result.AddResultRule("titleanalyzer_no_title_tag", ResultType.Error);
+                }
+                else if (titleTags.Count() > 1)
+                {
+                    result.AddResultRule("titleanalyzer_multiple_title_tags", ResultType.Error);
+                }
+                else
+                {
+                    var firstTitleTag = titleTags.FirstOrDefault();
+                    if (firstTitleTag != null)
                     {
-                        result.AddResultRule("titleanalyzer_no_title_value", ResultType.Error);
-                    }
-                    else
-                    {
-                        titleValue = titleValue.Trim();
+                        var titleValue = firstTitleTag.InnerText;
 
-                        if (titleValue.Length > 60)
+                        if (string.IsNullOrWhiteSpace(titleValue))
                         {
-                            result.AddResultRule("titleanalyzer_title_too_long", ResultType.Warning);
+                            result.AddResultRule("titleanalyzer_no_title_value", ResultType.Error);
                         }
+                        else
+                        {
+                            titleValue = titleValue.Trim();
 
-                        if (titleValue.Length < 10)
-                        {
-                            result.AddResultRule("titleanalyzer_title_too_short", ResultType.Warning);
-                        }
-                        else if (titleValue.Length < 40)
-                        {
-                            result.AddResultRule("titleanalyzer_title_less_than_40_characters", ResultType.Warning);
-                        }
+                            if (titleValue.Length > 60)
+                            {
+                                result.AddResultRule("titleanalyzer_title_too_long", ResultType.Warning);
+                            }
 
-                        if (titleValue.Length <= 60 && titleValue.Length >= 40)
-                        {
-                            result.AddResultRule("titleanalyzer_title_more_than_40_less_than_60_characters", ResultType.Success);
+                            if (titleValue.Length < 10)
+                            {
+                                result.AddResultRule("titleanalyzer_title_too_short", ResultType.Warning);
+                            }
+                            else if (titleValue.Length < 40)
+                            {
+                                result.AddResultRule("titleanalyzer_title_less_than_40_characters", ResultType.Warning);
+                            }
+
+                            if (titleValue.Length <= 60 && titleValue.Length >= 40)
+                            {
+                                result.AddResultRule("titleanalyzer_title_more_than_40_less_than_60_characters",
+                                    ResultType.Success);
+                            }
                         }
                     }
                 }
+            }
+            else
+            {
+                result.AddResultRule("titleanalyzer_no_head_tag", ResultType.Error);
             }
             return result;
         }
