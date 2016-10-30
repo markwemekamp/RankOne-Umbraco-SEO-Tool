1e22624402425e6ad45a2c5b169d6b6b9724f8be
Mark <mwemekamp@gmail.com>
2016-10-30 21:23:45 +0100

    Fixed translation bug in dashboardcontroller.js

--- src/RankOne.SEO.Tool/Web/UI/App_Plugins/RankOne/editors/dashboard/dashboard.controller.js
+++ src/RankOne.SEO.Tool/Web/UI/App_Plugins/RankOne/editors/dashboard/dashboard.controller.js
@@ -16,38 +16,38 @@
                 var url = '/umbraco/backoffice/rankone/RankOneApi/AnalyzeNode?id={id}&focusKeyword=' +
                     $scope.model.value.focusKeyword;
                 webresultService.GetResultFromEditorState(editorState.current, url)
-                    .then(function(response) {
+                    .then(function (response) {
 
-                            $scope.analyzeResults = response;
+                        $scope.analyzeResults = response;
 
-                            $scope.overallScore = $scope.analyzeResults.Score.OverallScore;
-                            $scope.errorCount = $scope.analyzeResults.Score.ErrorCount;
-                            $scope.warningCount = $scope.analyzeResults.Score.WarningCount;
-                            $scope.hintCount = $scope.analyzeResults.Score.HintCount;
-                            $scope.successCount = $scope.analyzeResults.Score.SuccessCount;
+                        $scope.overallScore = $scope.analyzeResults.Score.OverallScore;
+                        $scope.errorCount = $scope.analyzeResults.Score.ErrorCount;
+                        $scope.warningCount = $scope.analyzeResults.Score.WarningCount;
+                        $scope.hintCount = $scope.analyzeResults.Score.HintCount;
+                        $scope.successCount = $scope.analyzeResults.Score.SuccessCount;
 
-                            $scope.loading = false;
+                        $scope.loading = false;
 
-                            $scope.color = '#4db53c';
-                            if ($scope.overallScore < 33) {
-                                $scope.color = '#dd2222';
-                            } else if ($scope.overallScore < 66) {
-                                $scope.color = '#dd9d22';
-                            }
+                        $scope.color = '#4db53c';
+                        if ($scope.overallScore < 33) {
+                            $scope.color = '#dd2222';
+                        } else if ($scope.overallScore < 66) {
+                            $scope.color = '#dd9d22';
+                        }
 
-                            $("#score")
-                                .circliful({
-                                    percent: $scope.overallScore,
-                                    fontColor: '#000000',
-                                    foregroundColor: $scope.color,
-                                    percentageTextSize: 30
-                                });
+                        $("#score")
+                            .circliful({
+                                percent: $scope.overallScore,
+                                fontColor: '#000000',
+                                foregroundColor: $scope.color,
+                                percentageTextSize: 30
+                            });
 
-                        },
-                        function(message) {
-                            $scope.error = message;
-                            $scope.loading = false;
-                        });
+                    },
+                    function (message) {
+                        $scope.error = message;
+                        $scope.loading = false;
+                    });
             }
         }
 
@@ -57,7 +57,15 @@
                 show: true,
                 callback: function (data) {
                     $scope.model.value = data;
-                    notificationsService.warning(localizationService.localize("warning_changes_pending_title"), localizationService.localize("warning_changes_pending_text"));
+                    localizationService.localize("warning_changes_pending_title")
+                        .then(function (title) {
+                            localizationService.localize("warning_changes_pending_text")
+                                .then(function (text) {
+
+                                    notificationsService
+                                        .warning(title, text);
+                                });
+                        });
                 },
                 dialogData: {
                     configuration: $scope.model.value
