78a1ec15c5de9d376222d4f6b3c98b2deebb47b7
mwemekamp@gmail.com <mwemekamp@gmail.com>
2018-03-21 10:24:07 +0100

    Added unit test classes for minificationhelpers

---
+++ src/RankOne.Tests/Analyzers/CssMinificationAnalyzerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RankOne.Analyzers.Performance;
+using RankOne.Helpers;
+using System;
+
+namespace RankOne.Tests.Analyzers
+{
+    [TestClass]
+    public class CssMinificationAnalyzerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_OnExecuteWithNullParameterForMinificationHelper_ThrowArgumentNullException()
+        {
+            new CssMinificationAnalyzer(null, new CacheHelper(), new UrlHelper());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_OnExecuteWithNullParameterForCacheHelper_ThrowArgumentNullException()
+        {
+            new CssMinificationAnalyzer(new MinificationHelper(), null, new UrlHelper());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_OnExecuteWithNullParameterForUrlHelper_ThrowArgumentNullException()
+        {
+            new CssMinificationAnalyzer(new MinificationHelper(), new CacheHelper(), null);
+        }
+    }
+}
---
+++ src/RankOne.Tests/Analyzers/JavascriptMinificationAnalyzerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RankOne.Analyzers.Performance;
+using RankOne.Helpers;
+using System;
+
+namespace RankOne.Tests.Analyzers
+{
+    [TestClass]
+    public class JavascriptMinificationAnalyzerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_OnExecuteWithNullParameterForMinificationHelper_ThrowArgumentNullException()
+        {
+            new JavascriptMinificationAnalyzer(null, new CacheHelper(), new UrlHelper());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_OnExecuteWithNullParameterForCacheHelper_ThrowArgumentNullException()
+        {
+            new JavascriptMinificationAnalyzer(new MinificationHelper(), null, new UrlHelper());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_OnExecuteWithNullParameterForUrlHelper_ThrowArgumentNullException()
+        {
+            new JavascriptMinificationAnalyzer(new MinificationHelper(), new CacheHelper(), null);
+        }
+    }
+}
