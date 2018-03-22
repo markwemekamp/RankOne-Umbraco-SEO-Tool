dbac43f3645954aeacef6ef7b5c7a9a63c3a896b
mwemekamp@gmail.com <mwemekamp@gmail.com>
2018-03-22 20:14:16 +0100

    Added unit test for minificationhelper, fixes bug in minificationhelper

--- src/RankOne.SEO.Tool/Helpers/MinificationHelper.cs
+++ src/RankOne.SEO.Tool/Helpers/MinificationHelper.cs
@@ -12,7 +12,7 @@ namespace RankOne.Helpers
             var totalCharacters = content.Length;
             var lines = content.Split('\n').Length;
             var ratio = totalCharacters / lines;          // ratio characters per line
-            return ratio < densityRatio;
+            return ratio > densityRatio;
         }
     }
 }
---
+++ src/RankOne.Tests/Helpers/MinificationHelperTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RankOne.Helpers;
+using System;
+
+namespace RankOne.Tests.Helpers
+{
+    [TestClass]
+    public class MinificationHelperTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IsMinified_OnExecuteWithNullParameter_ThrowsException()
+        {
+            var minificationHelper = new MinificationHelper();
+            minificationHelper.IsMinified(null);
+        }
+
+        [TestMethod]
+        public void IsMinified_OnExecuteUnminifiedCode_ReturnsFalse()
+        {
+            var input = @"<script>
+                            alert('test');
+                            </script>";
+
+
+
+            var minificationHelper = new MinificationHelper();
+            var minified = minificationHelper.IsMinified(input);
+
+            Assert.IsFalse(minified);
+        }
+
+        [TestMethod]
+        public void IsMinified_OnExecuteWithMinifiedCode_ReturnsTrue()
+        {
+
+            var minificationHelper = new MinificationHelper();
+            var minified = minificationHelper.IsMinified(input);
+
+            Assert.IsTrue(minified);
+        }
+    }
+}
